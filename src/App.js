7d9ad09c0abee49b298c8fe5e576e9eab6caee22
Joseph <notseph8556@gmail.com>
2022-02-01 16:22:50 +0900

    v22.02.01

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useRef, useState, useReducer } from 'react';
+import { MdArrowRightAlt } from 'react-icons/md';
 import './App.css';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
+function createBulkTodos () {
+  const array = [];
+  for(let i = 0; i <= 23; i++){
+    array.push({
+      id:i,
+      text:'할일',
+      checked:false,
+    })
+  }
+  return array;
+}
+
+
+function todoReducer(todos, action){
+  switch (action.type){
+    case'INSERT' :
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter( todo => todo = todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map( todo =>
+        todo.id === action.id ? {...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '리액트의 기초 알아보기',
-      checked: false,
-    },
-  ]);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   const nextId = useRef(4);
 
@@ -31,20 +44,16 @@ function App() {
       text,
       chacked:false,
     };
-    setTodos(todos.concat(todo));
+    dispatch({ type:'INSERT' , todo })
     nextId.current += 1;
   },[todos]);
 
   const onRemove = useCallback((id)=> {
-    setTodos(todos.filter(todo => todo.id !== id))
+    dispatch({ type:'REMOVE' , id })
   },[todos]);
 
   const onToggle = useCallback((id)=>{
-    setTodos(
-      todos.map(todo =>
-        todo.id === id ? {...todo, checked: !todo.checked} : todo,
-      ),
-    )
+    dispatch({ type:'TOGGLE', id })
   },[todos])
 
   return (
